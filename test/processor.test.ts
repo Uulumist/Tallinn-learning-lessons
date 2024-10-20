04f59f182a28df69cec1530561e9290de748a320
Uulumist <uulumist@gmail.com>
2024-10-20 15:18:44 +0300

    Homework 6 - added revokeConsent method to Processor class and unit tests to check functionality

--- test/processor.test.ts
+++ test/processor.test.ts
@@ -5,24 +5,42 @@ let user: User
 let processor: Processor
 
 beforeEach(() => {
-  user = new User('Tom', 'Yorke', 1234567, 'Tallinn')
+  user = new User('Tom', 'Yorke', 1234567, 'Tallinn', 18)
   processor = new Processor()
 })
 
-test('check new user undefined', () => {
+test('check that consent is undefined for new user', () => {
   expect(user.consentGiven).toBeUndefined()
 })
 
-test('check processor approve', () => {
+test('check that consent is given by user', () => {
   processor.givenConsent(user)
   expect(user.consentGiven).toBe(true)
 })
 
-test('check processor verify', () => {
+test('check that processor verified that user gave consent', () => {
   processor.givenConsent(user)
   expect(processor.checkConsent(user)).toBeTruthy()
 })
 
-test('check consent declined', () => {
+test('check that processor verified that user didnt give consent', () => {
   expect(processor.checkConsent(user)).toBeFalsy()
+})
+
+test('check that consent is revoked if users age <18', () => {
+  user.age = 17
+  processor.givenConsent(user)
+  expect(processor.revokeConsent(user)).toBe(true)
+})
+
+test('check that consent is not revoked if users age >=18', () => {
+  user.age = 20
+  processor.givenConsent(user)
+  expect(processor.revokeConsent(user)).toBe(false)
+})
+
+test('check that consent is revoked if users age 0', () => {
+  user.age = 0
+  processor.givenConsent(user)
+  expect(processor.revokeConsent(user)).toBe(true)
 })
